c3839c7bef2a74345f58c91873912ee4cccf2181
Brett Morrison <b@brettmorrison.com>
2021-11-22 15:08:26 -0800

    AD-19 Improved OpenApi[] annotations for User model. Removed unused import.

--- TrueVote.Api/Services/Startup.cs
+++ TrueVote.Api/Services/Startup.cs
@@ -2,7 +2,6 @@ using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
