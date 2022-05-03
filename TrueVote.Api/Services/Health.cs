cdb487080538f835db203f8e54d6d98ffec8ef02
Brett Morrison <b@brettmorrison.com>
2022-05-03 14:11:11 -0700

    AD-18 Added timed function (5 minutes) to keep the function app alive

---
+++ TrueVote.Api/Services/Health.cs
@@ -0,0 +1,20 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using TrueVote.Api.Helpers;
+
+namespace TrueVote.Api.Services
+{
+    public class Health : LoggerHelper
+    {
+        public Health(ILogger log): base(log)
+        {
+        }
+
+        [FunctionName("HealthTimer")]
+        public void Run([TimerTrigger("*/5 * * * *")] TimerInfo timerInfo)
+        {
+            _log.LogInformation($"HealthTimer trigger function {timerInfo.Schedule} executed at: {DateTime.Now.ToUniversalTime().ToString("dddd, MMM dd, yyyy HH:mm:ss")}");
+        }
+    }
+}
