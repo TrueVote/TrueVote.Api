c791200b74e43eb7f621bf2ff6dff85af8ffc0ef
Brett Morrison <b@brettmorrison.com>
2022-06-09 07:45:22 -0700

    AD-24 Rename GraphQLFunction for consistency

--- TrueVote.Api/Services/GraphQL.cs
+++ TrueVote.Api/Services/GraphQL.cs
@@ -12,16 +12,16 @@ using TrueVote.Api.Helpers;
 
 namespace TrueVote.Api.Services
 {
-    public class GraphQLFunction : LoggerHelper
+    public class GraphQL : LoggerHelper
     {
-        public GraphQLFunction(ILogger log, TelegramBot telegramBot) : base(log, telegramBot)
+        public GraphQL(ILogger log, TelegramBot telegramBot) : base(log, telegramBot)
         {
         }
 
-        [FunctionName("GraphQLHttpFunction")]
+        [FunctionName("GraphQL")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [OpenApiOperation(operationId: "GraphQLFunction", tags: new[] { "GraphQL" })]
+        [OpenApiOperation(operationId: "GraphQL", tags: new[] { "GraphQL" })]
         [OpenApiSecurity("oidc_auth", SecuritySchemeType.OpenIdConnect, OpenIdConnectUrl = "https://login.microsoftonline.com/{tenant_id}/v2.0/.well-known/openid-configuration", OpenIdConnectScopes = "openid,profile")]
         [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(string), Description = "GraphQL Query Entry Point")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "Returns GraphQL Query Response")]
@@ -29,11 +29,11 @@ namespace TrueVote.Api.Services
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "graphql/{**slug}")] HttpRequest req, [GraphQL] IGraphQLRequestExecutor executor)
         {
-            LogDebug("HTTP trigger - GraphQLHttpFunction:Begin");
+            LogDebug("HTTP trigger - GraphQL:Begin");
 
             var ret = executor.ExecuteAsync(req);
 
-            LogDebug("HTTP trigger - GraphQLHttpFunction:End");
+            LogDebug("HTTP trigger - GraphQL:End");
 
             return await ret;
         }
