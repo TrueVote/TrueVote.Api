ed650bc11413bfc818e287a2c0acdf26a950a142
Brett Morrison <b@brettmorrison.com>
2023-09-26 16:00:07 -0700

    Added comments to Version.cs

--- TrueVote.Api/Version.cs
+++ TrueVote.Api/Version.cs
@@ -1,3 +1,8 @@
+/*
+ * This is a static template class as part of the CI/CD process. build-version.sh overwrites the values with current build info.
+ * Version.cs is in .gitignore. To change this file, be sure and remove it from .gitignore, change it,
+ * commit it, and then put the .gitignore entry back.
+ */
 namespace TrueVote.Api
 {
     public static class VersionInfo
