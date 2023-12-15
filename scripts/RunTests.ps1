6bbb0bfe8adb9fed4d74a7f5fd943afaddedcf10
Brett Morrison <b@brettmorrison.com>
2023-12-14 16:32:58 -0800

    Refactored coverlet and coverralls for simplicity and proper ignoring of generated files

--- scripts/RunTests.ps1
+++ scripts/RunTests.ps1
@@ -1,27 +1,15 @@
 param([String]$ci="false") 
 
 # Run the test coverage
-$TestOutput = dotnet test --verbosity normal --collect:"XPlat Code Coverage" --settings:coverlet.runsettings /p:threshold=98 /p:thresholdType=line /p:thresholdStat=total /p:CollectCoverage=true
+$TestOutput = dotnet coverlet "TrueVote.Api.Tests\bin\Debug\net8.0\TrueVote.Api.Tests.dll" --target "dotnet" --targetargs "test --verbosity normal --no-build" --format cobertura --output TrueVote.Api.Tests/TestResults/ --threshold=100 --threshold-type=line --threshold-stat=total --exclude-by-file "**.g.cs"
 
 Write-Host $TestOutput
 
 # ci variable is set in .github/workflows/truevote-api-version.yml. When run locally, it will fall through here, pass or fail
-if ($ci -eq "true" -and $TestOutput -clike "*FAILED*") {
+if ($ci -eq "true" -and ($TestOutput -clike "*below the specified*" -or $TestOutput -clike "*FAILED*")) {
 	Write-Host "Failed. Exiting"
 	exit -1
 }
 
-# Find the generated GUID in the path
-$TestReports = $TestOutput | Select-String coverage.cobertura.xml | ForEach-Object { $_.Line.Trim() }
-
-# Replace backslashes with slashes. This helps it run on both Windows and Unix
-$TestReports = $TestReports.Replace('\', '/')
-
-# Parse off the path
-$TestReportsPath = $TestReports.Substring(0, $TestReports.LastIndexOf('/'))
-
-# Copy the coverage file up one directory
-Copy-Item $TestReportsPath/*.* -Destination 'TrueVote.Api.Tests/TestResults'
-
 # Generate the HTML Report
-dotnet reportgenerator "-reports:$TestReports" "-targetdir:TrueVote.Api.Tests/coverage-html" "-reporttype:Html"
+dotnet reportgenerator "-reports:TrueVote.Api.Tests/TestResults/coverage.cobertura.xml" "-targetdir:TrueVote.Api.Tests/coverage-html" "-reporttype:Html"
