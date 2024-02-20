1437ac644f194d8e70a29ddbb576c5ebc712aded
Brett Morrison <b@brettmorrison.com>
2024-02-20 11:50:10 -0800

    AD-80 Extracated out chained variables into instances

--- TrueVote.Api/Startup.cs
+++ TrueVote.Api/Startup.cs
@@ -175,7 +175,9 @@ namespace TrueVote.Api
     {
         public static void Main()
         {
-            var host = new HostBuilder().ConfigureFunctionsWorkerDefaults().ConfigureServices(s =>
+            var hostBuilder = new HostBuilder();
+            var hostBuilderDefaults = hostBuilder.ConfigureFunctionsWorkerDefaults();
+            var hostBuilderServices = hostBuilderDefaults.ConfigureServices(s =>
             {
                 s.AddApplicationInsightsTelemetryWorkerService();
                 s.ConfigureFunctionsApplicationInsights();
@@ -199,7 +201,8 @@ namespace TrueVote.Api
                 s.TryAddScoped<IValidator, Validator>();
 
                 ConfigureServices(s).BuildServiceProvider(true);
-            }).Build();
+            });
+            var host = hostBuilderServices.Build();
 
             host.Run();
         }
