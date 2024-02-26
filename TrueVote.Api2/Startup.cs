2105a4c92b9c9dc039085b9e0f94ccaa1db59ad0
Brett Morrison <b@brettmorrison.com>
2024-02-26 15:55:03 -0800

    AD-105 Scaffolding out .NET Core discrete instance (not Azure Function)

---
+++ TrueVote.Api2/Startup.cs
@@ -0,0 +1,34 @@
+#pragma warning disable IDE0058 // Expression value is never used
+namespace TrueVote.Api2
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
+#pragma warning restore IDE0058 // Expression value is never used
