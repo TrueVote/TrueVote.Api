787e3263974dbd980a1ce73677aad59855a69575
Brett Morrison <b@brettmorrison.com>
2024-02-26 14:32:37 -0800

    AD-105 Scaffolding out .NET Core discrete instance (not Azure Function)

---
+++ TrueVote.Api2/Program.cs
@@ -0,0 +1,36 @@
+
+namespace TrueVote.Api2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            _ = builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            _ = builder.Services.AddEndpointsApiExplorer();
+            _ = builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                _ = app.UseSwagger();
+                _ = app.UseSwaggerUI();
+            }
+
+            _ = app.UseHttpsRedirection();
+
+            _ = app.UseAuthorization();
+
+
+            _ = app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
