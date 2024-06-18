6150c65d17880cff501761949b7d5e9421766ae7
Brett Morrison <b@brettmorrison.com>
2024-06-18 14:53:06 -0700

    AD-99 Added comment reference to DTO ticket

--- TrueVote.Api/Services/Ballot.cs
+++ TrueVote.Api/Services/Ballot.cs
@@ -52,7 +52,7 @@ namespace TrueVote.Api.Services
 
             var validationResults = new List<ValidationResult>();
             var validationContext = new ValidationContext(bindSubmitBallotModel);
-            validationContext.Items["IsBallot"] = true;
+            validationContext.Items["IsBallot"] = true; // TODO https://truevote.atlassian.net/browse/AD-113
             if (!RecursiveValidator.TryValidateObjectRecursive(bindSubmitBallotModel, validationContext, validationResults))
             {
                 var errorDictionary = RecursiveValidator.GetValidationErrorsDictionary(validationResults);
