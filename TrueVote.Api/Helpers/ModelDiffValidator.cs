4637e8ff8dfbce857c0e238b0a7a1527a168a238
Brett Morrison <b@brettmorrison.com>
2024-07-17 16:46:52 -0700

    AD-95 Optimized ModelDiff further for latest C#

--- TrueVote.Api/Helpers/ModelDiffValidator.cs
+++ TrueVote.Api/Helpers/ModelDiffValidator.cs
@@ -2,162 +2,155 @@ using System.Collections;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
-#pragma warning disable IDE0046 // Convert to conditional expression
-namespace TrueVote.Api.Helpers
+namespace TrueVote.Api.Helpers;
+
+public static class ModelDiffExtensions
 {
-    public static class ModelDiffExtensions
+    public static Dictionary<string, (object? OldValue, object? NewValue)> ModelDiff<T>(this T? a, T? b, string prefix = "") where T : class
     {
-        public static Dictionary<string, (object? OldValue, object? NewValue)> ModelDiff<T>(this T? a, T? b, string prefix = "") where T : class
+        return CompareObjects(a, b, prefix);
+    }
+
+    internal static Dictionary<string, (object? OldValue, object? NewValue)> CompareObjects(object? a, object? b, string prefix)
+    {
+        var differences = new Dictionary<string, (object? OldValue, object? NewValue)>();
+
+        if (a is null && b is null)
+            return differences;
+
+        if (a is null || b is null)
         {
-            return CompareObjects(a, b, prefix);
+            differences[CreateKey(prefix)] = (a, b);
+            return differences;
         }
 
-        internal static Dictionary<string, (object? OldValue, object? NewValue)> CompareObjects(object? a, object? b, string prefix)
+        var type = a.GetType();
+
+        return type switch
         {
-            var differences = new Dictionary<string, (object? OldValue, object? NewValue)>();
+            var t when IsSimpleType(t) => CompareSimpleTypes(a, b, prefix),
+            var t when typeof(IEnumerable).IsAssignableFrom(t) && t != typeof(string) => CompareEnumerables(a as IEnumerable, b as IEnumerable, prefix),
+            _ => CompareComplexTypes(a, b, prefix)
+        };
+    }
 
-            if (a == null && b == null)
-                return differences;
+    internal static Dictionary<string, (object? OldValue, object? NewValue)> CompareSimpleTypes(object a, object b, string prefix)
+    {
+        var differences = new Dictionary<string, (object? OldValue, object? NewValue)>();
 
-            if (a == null || b == null)
-            {
-                differences[CreateKey(prefix)] = (a, b);
-                return differences;
-            }
-
-            var type = a.GetType();
-
-            if (IsSimpleType(type))
-            {
-                return CompareSimpleTypes(a, b, prefix);
-            }
-
-            if (typeof(IEnumerable).IsAssignableFrom(type) && type != typeof(string))
-            {
-                return CompareEnumerables(a as IEnumerable, b as IEnumerable, prefix);
-            }
-
-            return CompareComplexTypes(a, b, prefix);
-        }
-
-        internal static Dictionary<string, (object? OldValue, object? NewValue)> CompareSimpleTypes(object a, object b, string prefix)
+        if (a is DateTime || a is DateTime?)
         {
-            var differences = new Dictionary<string, (object? OldValue, object? NewValue)>();
-
-            if (a is DateTime || a is DateTime?)
-            {
-                if (!AreDateTimesEqual(a as DateTime?, b as DateTime?))
-                {
-                    differences[CreateKey(prefix)] = (a, b);
-                }
-            }
-            else if (!Equals(a, b))
+            if (!AreDateTimesEqual(a as DateTime?, b as DateTime?))
             {
                 differences[CreateKey(prefix)] = (a, b);
             }
-
-            return differences;
+        }
+        else if (!Equals(a, b))
+        {
+            differences[CreateKey(prefix)] = (a, b);
         }
 
-        internal static string JoinListItems(IEnumerable<string?> items)
+        return differences;
+    }
+
+    internal static string JoinListItems(IEnumerable<string?> items)
+    {
+        return string.Join(",", items.Select(i => i ?? string.Empty));
+    }
+
+    [ExcludeFromCodeCoverage] // So strange that this needs to be uncovered, but it does.
+    public static string CreateKey(string prefix)
+    {
+        return prefix.TrimEnd('.');
+    }
+
+    public static Dictionary<string, (object? OldValue, object? NewValue)> CompareEnumerables(IEnumerable? a, IEnumerable? b, string prefix)
+    {
+        var differences = new Dictionary<string, (object? OldValue, object? NewValue)>();
+
+        var listA = a?.Cast<object>().ToList() ?? [];
+        var listB = b?.Cast<object>().ToList() ?? [];
+
+        var maxCount = Math.Max(listA.Count, listB.Count);
+
+        for (var i = 0; i < maxCount; i++)
         {
-            return string.Join(",", items.Select(i => i ?? string.Empty));
-        }
+            var itemA = i < listA.Count ? listA[i] : null;
+            var itemB = i < listB.Count ? listB[i] : null;
 
-        [ExcludeFromCodeCoverage] // So strange that this needs to be uncovered, but it does.
-        public static string CreateKey(string prefix)
-        {
-            return prefix.TrimEnd('.');
-        }
+            if (itemA is null && itemB is null) continue;
 
-        public static Dictionary<string, (object? OldValue, object? NewValue)> CompareEnumerables(IEnumerable? a, IEnumerable? b, string prefix)
-        {
-            var differences = new Dictionary<string, (object? OldValue, object? NewValue)>();
-
-            var listA = a?.Cast<object>().ToList() ?? new List<object>();
-            var listB = b?.Cast<object>().ToList() ?? new List<object>();
-
-            var maxCount = Math.Max(listA.Count, listB.Count);
-
-            for (var i = 0; i < maxCount; i++)
+            if (itemA is null || itemB is null || !Equals(itemA, itemB))
             {
-                var itemA = i < listA.Count ? listA[i] : null;
-                var itemB = i < listB.Count ? listB[i] : null;
-
-                if (itemA == null && itemB == null) continue;
-
-                if (itemA == null || itemB == null || !Equals(itemA, itemB))
+                if (itemA is not null && itemB is not null && !IsSimpleType(itemA.GetType()))
                 {
-                    if (itemA != null && itemB != null && !IsSimpleType(itemA.GetType()))
+                    var nestedDifferences = CompareObjects(itemA, itemB, $"{prefix}[{i}].");
+                    foreach (var kvp in nestedDifferences)
                     {
-                        var nestedDifferences = CompareObjects(itemA, itemB, $"{prefix}[{i}].");
-                        foreach (var kvp in nestedDifferences)
-                        {
-                            differences[kvp.Key] = kvp.Value;
-                        }
-                    }
-                    else
-                    {
-                        var displayA = listA.Select(item => item != null && !IsSimpleType(item.GetType()) ? "ComplexType" : item?.ToString());
-                        var displayB = listB.Select(item => item != null && !IsSimpleType(item.GetType()) ? "ComplexType" : item?.ToString());
-                        differences[CreateKey(prefix)] = (JoinListItems(displayA), JoinListItems(displayB));
-                        return differences;
+                        differences[kvp.Key] = kvp.Value;
                     }
                 }
+                else
+                {
+                    var displayA = listA.Select(item => item is not null && !IsSimpleType(item.GetType()) ? "ComplexType" : item?.ToString());
+                    var displayB = listB.Select(item => item is not null && !IsSimpleType(item.GetType()) ? "ComplexType" : item?.ToString());
+                    differences[CreateKey(prefix)] = (JoinListItems(displayA), JoinListItems(displayB));
+                    return differences;
+                }
             }
-
-            return differences;
         }
 
-        public static Dictionary<string, (object? OldValue, object? NewValue)> CompareComplexTypes(object a, object b, string prefix)
-        {
-            var differences = new Dictionary<string, (object? OldValue, object? NewValue)>();
-            var properties = a.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        return differences;
+    }
 
-            foreach (var property in properties)
+    public static Dictionary<string, (object? OldValue, object? NewValue)> CompareComplexTypes(object a, object b, string prefix)
+    {
+        var differences = new Dictionary<string, (object? OldValue, object? NewValue)>();
+        var properties = a.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var property in properties)
+        {
+            object? valueA, valueB;
+            try
             {
-                object? valueA, valueB;
-                try
-                {
-                    valueA = property.GetValue(a);
-                    valueB = property.GetValue(b);
-                }
-                catch (Exception)
-                {
-                    continue;
-                }
-
-                var propertyName = string.IsNullOrEmpty(prefix) ? property.Name : $"{prefix}{property.Name}";
-                var propertyDifferences = CompareObjects(valueA, valueB, propertyName);
-
-                foreach (var kvp in propertyDifferences)
-                {
-                    differences[kvp.Key] = kvp.Value;
-                }
+                valueA = property.GetValue(a);
+                valueB = property.GetValue(b);
+            }
+            catch (Exception)
+            {
+                continue;
             }
 
-            return differences;
+            var propertyName = string.IsNullOrEmpty(prefix) ? property.Name : $"{prefix}{property.Name}";
+            var propertyDifferences = CompareObjects(valueA, valueB, propertyName);
+
+            foreach (var kvp in propertyDifferences)
+            {
+                differences[kvp.Key] = kvp.Value;
+            }
         }
 
-        internal static bool IsSimpleType(Type type)
-        {
-            return type.IsPrimitive
-                || type == typeof(string)
-                || type == typeof(decimal)
-                || type == typeof(DateTime)
-                || type == typeof(DateTimeOffset)
-                || type == typeof(TimeSpan)
-                || type == typeof(Guid)
-                || type.IsEnum
-                || Nullable.GetUnderlyingType(type) != null;
-        }
+        return differences;
+    }
 
-        public static bool AreDateTimesEqual(DateTime? a, DateTime? b)
-        {
-            return a == b || (a.HasValue && b.HasValue && a.Value.Date == b.Value.Date &&
-                   a.Value.Hour == b.Value.Hour && a.Value.Minute == b.Value.Minute &&
-                   a.Value.Second == b.Value.Second);
-        }
+    internal static bool IsSimpleType(Type type)
+    {
+        return type.IsPrimitive
+               || type == typeof(string)
+               || type == typeof(decimal)
+               || type == typeof(DateTime)
+               || type == typeof(DateTimeOffset)
+               || type == typeof(TimeSpan)
+               || type == typeof(Guid)
+               || type.IsEnum
+               || Nullable.GetUnderlyingType(type) is not null;
+    }
+
+    // Can't do a a != b comparison because that will compare down to the millisecond and microsecond. This only compares down to the second
+    public static bool AreDateTimesEqual(DateTime? a, DateTime? b)
+    {
+        return a == b || (a.HasValue && b.HasValue && a.Value.Date == b.Value.Date &&
+                          a.Value.Hour == b.Value.Hour && a.Value.Minute == b.Value.Minute &&
+                          a.Value.Second == b.Value.Second);
     }
 }
-#pragma warning restore IDE0046 // Convert to conditional expression
